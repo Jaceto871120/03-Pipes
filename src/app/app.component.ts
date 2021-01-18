b304c8d497f6a7875ba79c49368510150a3152a4
Jair Celis <jair.c@montechelo.com.co>
2021-01-17 21:26:54 -0500

    Clase terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = 'Capitan America';
+  nombre2 = 'jAiR CeLis';
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: '500'
+  };
+
+  idioma = 'es-CO';
+
+  videoUrl = 'https://www.youtube.com/embed/DIlbsY-7aDQ';
+
+  activar = true;
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('Llego la data');
+    }, 4500);
+  });
+  fecha = new Date();
+
+  espanol() {
+    this.idioma = 'es-CO';
+  }
+
+  ingles() {
+    this.idioma = 'es-US';
+  }
+
+  frances() {
+    this.idioma = 'fr';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeCO from '@angular/common/locales/es-CO';
+import localeFR from '@angular/common/locales/fr';
+import localeUS from '@angular/common/locales/en-US-POSIX';
+
+registerLocaleData(localeCO, 'es-CO');
+registerLocaleData(localeFR, 'fr');
+registerLocaleData(localeUS, 'es-US');
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { OcultarStringPipe } from './pipes/ocultar-string.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    OcultarStringPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-CO'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
